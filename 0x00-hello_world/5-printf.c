e1325c7170e8ef4776262f44e11b16cd9529b503
nickaj1 <www.nickanderson99@gmail.com>
2023-02-17 01:06:04 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: Always 0
  */
 int main(void)
-{ printf("with proper grammer, but the outcome is a piece of art,");
+{ printf ("with proper grammer, but the outcome is a piece of art,");
 return (0);
 }
