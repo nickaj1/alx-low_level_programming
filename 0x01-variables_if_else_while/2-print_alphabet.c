8f5ca4120989173f4b50a1f131e64f719cef7ef9
nickaj1 <www.nickanderson99@gmail.com>
2023-02-18 11:11:22 -0800

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{ char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
